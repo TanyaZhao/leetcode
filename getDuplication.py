f48d7cee8c88170f84131ab868715783dd8d5b12
TanyaZhao <zty0331@163.com>
2018-03-06 12:50:14 +0800

    add new files

---
+++ getDuplication.py
@@ -0,0 +1,46 @@
+# coding:utf-8
+# 不修改数组，找出重复的数字
+def getDuplication(nums, length):
+    if nums is None or length <= 0:
+        return -1
+
+    start = 1
+    end = length - 1
+    while start <= end:
+        middle = (end - start)/2 + start
+        # 统计start到middle中数字出现的次数
+        count = countRange(nums, length, start ,middle)
+        if end == start:
+            if count > 1:
+                return start
+            else:
+                break
+
+        if count > middle-start+1:
+            end = middle
+        else:
+            start = middle + 1
+
+    return -1
+
+
+def countRange(nums, length, start, end):
+    '''
+    统计start到end中数字出现的次数
+    :param nums:
+    :param start:
+    :param end:
+    :return:
+    '''
+    if nums is None:                                                                                                                                                        
+        return 0
+
+    count = 0
+    for i in range(length):
+        if nums[i] >= start and nums[i] <= end:
+            count = count + 1
+
+    return count
+
+
+print getDuplication([2,3,5,4,3,2,6,7], 8)
---
+++ two_sum.py
@@ -0,0 +1,30 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    # target_list = []
+    # dict_num = {}
+    # for index, num in enumerate(nums):
+    #     dict_num[num] = index
+    #
+    # for num, index in dict_num.items():
+    #     diff = target - num
+    #     if dict_num.has_key(diff):
+    #         index2 = dict_num[diff]
+    #         target_list.append(index)
+    #         target_list.append(dict_num[diff])
+    #         target_list.sort()
+    #         return target_list
+    #     else:
+    #         continue
+    dict = {}
+    for i in xrange(len(nums)):
+        x = nums[i]
+        if target - x in dict:
+            return [dict[target - x], i]
+        dict[x] = i
+
+
+print twoSum([3,5,3], 6)
